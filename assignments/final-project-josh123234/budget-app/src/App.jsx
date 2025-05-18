dc51eaaf60482bec53535ea90bbac3dad347d49d
Josh <jhoffmansenn@gmail.com>
2025-05-18 00:19:18 -0400

    hashrouter for prod

--- assignments/final-project-josh123234/budget-app/src/App.jsx
+++ assignments/final-project-josh123234/budget-app/src/App.jsx
@@ -1,12 +1,14 @@
 import "./budget-app.css"
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import {Nav} from "./nav";
 import {MyRouteApp} from "./routes"
 
 function App() {
 
     return (
-        <Router basename="/dgmd-e28-scale/assignments/final-project-josh123234/budget-app">
+        // <Router basename="/dgmd-e28-scale/assignments/final-project-josh123234/budget-app/">
+        <Router>
             <Nav />
             <MyRouteApp />
         </Router>
