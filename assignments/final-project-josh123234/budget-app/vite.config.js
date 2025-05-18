2c193ea8048758e1be018db4240c55d8e25471df
Josh <jhoffmansenn@gmail.com>
2025-05-17 22:36:40 -0400

    additional files and homepage and base edit

--- assignments/final-project-josh123234/budget-app/vite.config.js
+++ assignments/final-project-josh123234/budget-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dgmd-e28-scale/assignments/final-project-josh123234/"
+  base: "/dgmd-e28-scale/assignments/final-project-josh123234/budget-app"
 })
